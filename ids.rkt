fc502cbd667403dfea3854dab8a6bcfbd3a99a8a
John Clements <clements@racket-lang.org>
2014-12-10 16:12:02 -0800

    update sensor names

--- ids.rkt
+++ ids.rkt
@@ -6,6 +6,24 @@
 
 (define-type ID
   (U "s-temp-lr"
-     "s-temp-kit"))
+     "s-occ-lr"
+     "s-amb-lr"
+     "c-light-lr"
+     "s-light-lr"
+     "s-temp-bath"
+     "s-occ-bath"
+     "s-amb-bath"
+     "c-light-bath"
+     "s-light-bath"
+     "s-temp-kit"
+     "s-occ-kit"
+     "s-amb-kit"
+     "c-light-kit"
+     "s-light-kit"
+     "s-temp-bed"
+     "s-occ-bed"
+     "s-amb-bed"
+     "c-light-bed"
+     "s-light-bed"))
 
 (define-predicate ID? ID)
