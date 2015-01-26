bc45897a5af84d921dca300185ffcc8902488f86
Gabe Villalobos <gabriel_villalobos@outlook.com>
2015-01-25 19:49:01 -0800

    Removed unnecessary files. U[dated JSHint file.

--- index.js
+++ index.js
@@ -5,21 +5,29 @@ var io = require('socket.io')(http);
 
 var port = 3001;
 
-app.get('/', function(req, res){
+app.get('/', function (req, res) {
+  'use strict';
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
+io.on('connection', function (socket) {
+  'use strict';
   console.log('Connection Successful!');
+
+  socket.on('disconnect', function () {
+    console.log('Client Disconnected');
+  });
+    
+  socket.on('connect_failed', function (data) {
+    console.log(data || 'connect_failed');
+  });
 
   socket.emit('test', { receivers: 'everyone'});
 
-  socket.on('disconnect', function() {
-    console.log('Client Disconnected');
-  });
 });
 
-http.listen(port, function(){
+http.listen(port, function () {
+  'use strict';
   console.log('listening on *:' + port);
 });
 
@@ -27,5 +35,3 @@ http.listen(port, function(){
 module.exports = function (str) {
   console.log(str || 'Rainbow');
 };
-
-
