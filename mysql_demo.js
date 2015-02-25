aeede0b7a71af0a8ab7caeca362edd5e35e9c721
Gabe Villalobos <gabriel_villalobos@outlook.com>
2015-02-24 21:49:49 -0800

    Added DB connection information

---
+++ mysql_demo.js
@@ -0,0 +1,20 @@
+
+var db         = require('./dbConnect.js');
+var connection = db.initConnect();
+
+
+// Just some test data
+for (var i = 0; i < 10; i++)
+    db.addSensorEvent(connection, 'c-light-bath', i);
+
+db.getLastSensor(connection, function(err, result) {
+    if (err) {
+            // error handling code goes here
+            console.log("ERROR : ",err);            
+        } else {            
+            // code to execute on data retrieval
+            console.log("result from last sensor is : ", result);   
+        }    
+});
+
+connection.end();
