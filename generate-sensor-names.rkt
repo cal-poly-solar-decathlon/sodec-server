9da16b27982eb485b843a1b6b179ddea0a8577fa
John Clements <clements@racket-lang.org>
2015-03-09 07:51:37 -0700

    sodec room & sensor names

--- generate-sensor-names.rkt
+++ generate-sensor-names.rkt
@@ -3,19 +3,28 @@
 (define rooms'(("lr" "living room")
                ("bath" "bathroom")
                ("kit" "kitchen")
-               ("bed" "bedroom")))
+               ("bed" "bedroom")
+               ("mech" "mechanical room")
+               ("out" "outside north wall")))
+
+;- `hum` : Humidity : out, bed, bath, lr
+;- `amb` : Ambient Light: bed, mech, lr, bath
+;- `occ` : Occupancy sensors: bed, mech, lr, bath
+
+
 (define sensor-patterns
-  '(("s-temp" "the temperature in the ~a")
-    ("s-occ" "whether the ~a is occupied")
-    ("s-amb" "the ambient light level in the ~a")
-    ("c-light" "control of the lights in the ~a")
-    ("s-light" "a record of the lighting control events in the ~a")))
+  '(("s-temp" ("out" "bed" "bath" "lr") "the temperature in the ~a")
+    ("s-hum" ("out" "bed" "bath" "lr") "the humidity in the ~a")
+    ("s-occ" ("bed" "mech" "lr" "bath") "whether the ~a is occupied")
+    ("s-amb" ("bed" "mech" "lr" "bath") "the ambient light level in the ~a")
+    ("c-light" ("bed" "mech" "lr" "bath" "kit") "control of the lights in the ~a")
+    #;("s-light" "a record of the lighting control events in the ~a")))
 
 (for-each
  display
-(for*/list ([r (in-list rooms)]
-            [pat (in-list sensor-patterns)])
-  (~a "- `"(first pat)"-"(first r)"` : "(format (second pat) (second r))"\n")))
+(for*/list ([pat (in-list sensor-patterns)]
+            [r (in-list (second pat))])
+  (~a "- `"(first pat)"-"r"` : "(format (third pat) (second (assoc r rooms)))"\n")))
 
 (for*/list ([r (in-list rooms)]
             [pat (in-list sensor-patterns)])
