5c97c0d5d167641b3ee21bc56763b1ca4ec4db1b
Gabriel Villalobos <gabeVillalobos@Gabriels-MacBook-Pro.local>
2015-03-15 13:53:29 -0700

    Added support for new API posting style

---
+++ routes/record-reading.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+router.post('/', function(req, res, next) {
+	var dev = req.query.device;
+	var value = req.query.status;
+
+	db.addSensorEvent(dev, value);
+});
+
+module.exports = router;
