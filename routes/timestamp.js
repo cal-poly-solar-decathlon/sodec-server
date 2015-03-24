4f919ec2c184cc2c5be81c2ff9e810dd810172ea
Gabe Villalobos <gabe.villalobos@gmail.com>
2015-03-24 13:12:57 -0700

    Added ping and timestamp endpoints

---
+++ routes/timestamp.js
@@ -0,0 +1,10 @@
+var express = require('express');
+var router = express.Router();
+
+router.get('/', function(req, res, next) {
+  var curTime = Math.floor(new Date().getTime() / 1000);
+  
+  res.send({"timestamp" : curTime});
+});
+
+module.exports = router;
