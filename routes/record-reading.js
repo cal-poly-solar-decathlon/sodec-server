26bdf29f0874fbf52a0dc60b9263acc429fa4d9a
Brian Yu <brianyu1125@gmail.com>
2015-03-30 22:03:41 -0700

    device checking in record-reading

--- routes/record-reading.js
+++ routes/record-reading.js
@@ -7,17 +7,24 @@ router.post('/', function(req, res, next) {
 	var value = req.body.status;
 	var okStatus = JSON.stringify('okay');
 
-
-	if(value)
-	{
-		db.addSensorEvent(dev, value);
-		res.status(200).send(okStatus);
-	}
-	else
-	{
-		console.log('Failed for device ' + dev);	
-		res.status(400).send({error: 'No value was defined for the device'});
-	}
+	db.findDevice(dev, function(err, result) {
+    	if (result != 0) {
+        	if(value)
+			{
+				db.addSensorEvent(dev, value);
+				res.status(200).send(okStatus);
+			}
+			else
+			{
+				console.log('Failed for device ' + dev);	
+				res.status(400).send({error: 'No value was defined for the device'});
+			}
+		}
+		else {
+			res.status(400).send({error: 'No device found'});
+		}
+	});
+	
 	
 });
 
