607cb04acf1278daaa3728a116d92b544402cc4b
John Clements <clements@racket-lang.org>
2015-04-20 11:55:02 -0700

    oops updated ids.rkt

--- ids.rkt
+++ ids.rkt
@@ -22,6 +22,21 @@
      "s-amb-lr"
      "s-amb-bath"
      "s-temp-testing-blackhole"
-     "s-temp-testing-empty"))
+     "s-temp-testing-empty"
+     "s-light-entry-bookend-1A"
+     "s-light-chandelier-1B"
+     "s-light-tv-light-2A"
+     "s-light-kitchen-uplight-3A"
+     "s-light-under-counter-3B"
+     "s-light-pendant-bar-lights-3C"
+     "s-light-bathroom-ambient-4A"
+     "s-light-mirror-4B"
+     "s-light-flexspace-uplight-5A"
+     "s-light-flexspace-cabinet-5B"
+     "s-light-bedroom-uplight-6A"
+     "s-light-bedroom-cabinet-6B"
+     "s-light-porch-lights-8A"
+     "s-light-uplights-and-pot-lights-8B"))
+
 
 (define-predicate ID? ID)
