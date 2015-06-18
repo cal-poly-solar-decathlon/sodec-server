75757a1598ae5652ca0de7b7d0cf76121b9c5738
John Clements <clements@racket-lang.org>
2015-06-18 15:45:40 -0700

    adjust ping response to be legal json per spec

--- routes/ping.js
+++ routes/ping.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/ping/', function(req, res, next) {
-  res.send("alive");
+  res.send('"alive"');
 });
 
 module.exports = router;
