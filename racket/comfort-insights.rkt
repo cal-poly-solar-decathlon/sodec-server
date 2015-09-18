11f2d1bb86a46644423d7a33304932b5e3cea281
John Clements <clements@racket-lang.org>
2015-09-18 09:35:54 -0700

    started on insights, punch list

---
+++ racket/comfort-insights.rkt
@@ -0,0 +1,11 @@
+#lang racket
+
+(require "data-model.rkt")
+
+;; generate forecast-based insights
+
+(define HUMIDITY-THRESHOLD 90.0)
+
+(define (generate-comfort-insights)
+  "The house is on fire. Maybe you'd be more comfortable outside?"
+  )
