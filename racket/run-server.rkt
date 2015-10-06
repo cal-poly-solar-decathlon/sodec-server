964a483f4348135a3ff0c2e05abcc2263396df98
John Clements <clements@racket-lang.org>
2015-10-06 11:37:14 -0700

    add data mirroring

--- racket/run-server.rkt
+++ racket/run-server.rkt
@@ -5,7 +5,8 @@
          "server.rkt"
          "egauge-monitor.rkt"
          "ip-ping.rkt"
-         "network-config.rkt")
+         "network-config.rkt"
+         (only-in "data-model.rkt" echo-data-to-host))
 
 (define-runtime-path here ".")
 (define-runtime-path htdocs "./htdocs")
@@ -20,14 +21,15 @@
 
 (start-forecast-monitor)
 
-(serve/servlet start
-               ;; I see... changing server root path means you need
-               ;; your own configuration files....
-               ;; #:server-root-path here
-               #:extra-files-paths (list htdocs)
-               #:servlet-regexp #px"^/srv/.*"
-	       #:launch-browser? #f
-               #:listen-ip #f
-               #:port 3000
-               #:log-file (build-path here "server.log")
-)
+(parameterize ([echo-data-to-host '("calpolysolardecathlon.org" 3000)])
+  (serve/servlet start
+                 ;; I see... changing server root path means you need
+                 ;; your own configuration files....
+                 ;; #:server-root-path here
+                 #:extra-files-paths (list htdocs)
+                 #:servlet-regexp #px"^/srv/.*"
+                 #:launch-browser? #f
+                 #:listen-ip #f
+                 #:port 3000
+                 #:log-file (build-path here "server.log")
+                 ))
